5b9a74002321cad0b5c82229e5f9868a8bf99d20
Mashhch <56107606+Mashhch@users.noreply.github.com>
2020-04-13 08:58:19 +0300

    Update f9.cpp

--- f9.cpp
+++ f9.cpp
@@ -10,7 +10,7 @@ char* f9(char* s1, char s[20])
     {
         s[i] = m[i];
     }
-    printf("\n f9 строка: ");
+    printf("\n f9 СЃС‚СЂРѕРєР°: ");
     for (int i = 0; i < 20; i++)
         printf("%c", s[i]);
     s1 = s;
