823984ac09b355fd70636864101f1b237f58528a
Mashhch <56107606+Mashhch@users.noreply.github.com>
2020-04-13 08:55:53 +0300

    Update f4.cpp

--- f4.cpp
+++ f4.cpp
@@ -10,7 +10,7 @@ char* f4(char* s1, char s[20])
     {
         s[i] = m[i];
     }
-    printf("\n f4 строка: ");
+    printf("\n f4 СЃС‚СЂРѕРєР°: ");
     for (int i = 0; i < 20; i++)
         printf("%c", s[i]);
     s1 = s;
